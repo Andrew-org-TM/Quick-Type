9137b45be2289c5ced5b46c6c8e3c2610dda4ec6
inathan44 <inathan44@yahoo.com>
2023-04-18 14:36:28 -0400

    set user info into redux state on login

--- src/store/slices/AuthSlice.ts
+++ src/store/slices/AuthSlice.ts
@@ -6,12 +6,14 @@ export interface InitAuthState {
   user: User;
   loading: boolean;
   error: string;
+  username: string;
 }
 
 const initialState: InitAuthState = {
   user: <User>{},
   loading: false,
   error: '',
+  username: '',
 };
 
 const AuthSlice = createSlice({
@@ -24,10 +26,19 @@ const AuthSlice = createSlice({
     logOut(state: InitAuthState) {
       state.user = <User>{};
     },
+    setUsername(state: InitAuthState, action: PayloadAction<string>): void {
+      state.username = action.payload;
+    },
+    setError(state: InitAuthState, action: PayloadAction<string>): void {
+      state.error = action.payload;
+    },
+    setLoading(state: InitAuthState, action: PayloadAction<boolean>): void {
+      state.loading = action.payload;
+    },
   },
 });
 
-export const { setUser, logOut } = AuthSlice.actions;
+export const { setUser, logOut, setUsername } = AuthSlice.actions;
 
 export const selectAuthUser = (state: RootState) => state.auth.user;
 
