5421a76ca3dcf6a359c19b371a340a5d9fd775f4
inathan44 <inathan44@yahoo.com>
2023-12-30 20:56:36 -0500

    Leaderboard styling updates, minor code fixes

--- src/App.tsx
+++ src/App.tsx
@@ -15,19 +15,21 @@ import About from './components/About';
 
 function App() {
   return (
-    <div className="mx-auto max-w-7xl text-emerald-600">
-      <NavBar />
-      <Routes>
-        <Route path="signup" element={<SignupForm />} />
-        <Route path="/" element={<InputForm />} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/results" element={<Results />} />
-        <Route path="/leaderboards" element={<Leaderboards />} />
-        <Route path="/account" element={<Account />} />
-        <Route path="/reset" element={<SendRecovery />} />
-        <Route path="/reset/newpassword" element={<ResetPassword />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+    <div className="min-h-screen bg-[#25282C]">
+      <div className="mx-auto max-w-7xl text-emerald-600">
+        <NavBar />
+        <Routes>
+          <Route path="signup" element={<SignupForm />} />
+          <Route path="/" element={<InputForm />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/results" element={<Results />} />
+          <Route path="/leaderboards" element={<Leaderboards />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/reset" element={<SendRecovery />} />
+          <Route path="/reset/newpassword" element={<ResetPassword />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
     </div>
   );
 }
--- src/components/Leaderboards.tsx
+++ src/components/Leaderboards.tsx
@@ -1,35 +1,44 @@
-import React, { useEffect } from 'react';
+import React, { Key, useCallback, useEffect } from 'react';
 import supabase from '../supabaseConfig';
 import { useNavigate, Link } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../store/hooks';
 import { selectAuthUser } from '../store/slices/AuthSlice';
-import {
-  DataGrid,
-  GridRowProps,
-  GridColDef,
-  GridCellParams,
-  GridTreeNode,
-} from '@mui/x-data-grid';
-import { makeStyles } from '@mui/material';
-import { setLeaderboards } from '../store/slices/leaderboardSlice';
 
-let columns: GridColDef[] = [
-  // { field: 'id' },
-  { field: 'id', headerName: 'Rank', flex: 1 },
-  { field: 'username', headerName: 'Username', flex: 1 },
-  { field: 'wpm', headerName: 'Wpm', flex: 1 },
-  { field: 'raw', headerName: 'Raw', flex: 1 },
-  { field: 'accuracy', headerName: 'Accuracy', flex: 1 },
-  { field: 'language', headerName: 'Language', flex: 1 },
-  // { field: 'timeelapsed', headerName: 'Time Elapsed', flex: 1 },
-  // { field: 'created_at', flex: 1, width: 150, headerName: 'Date' },
+import { setLeaderboards } from '../store/slices/leaderboardSlice';
+import {
+  Table,
+  TableHeader,
+  TableBody,
+  TableColumn,
+  TableRow,
+  TableCell,
+} from '@nextui-org/react';
+
+type LeaderboardRow = {
+  rank: number;
+  username: string;
+  wpm: number;
+  raw: number;
+  accuracy: number;
+  language: string;
+  [key: string]: string | number;
+};
+
+const columns = [
+  { name: 'Rank', uid: 'id' },
+  { name: 'Username', uid: 'username' },
+  { name: 'WPM', uid: 'wpm' },
+  { name: 'Raw', uid: 'raw' },
+  { name: 'Accuracy', uid: 'accuracy' },
+  { name: 'Language', uid: 'language' },
 ];
 
 const Leaderboards = () => {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
-  const user = useAppSelector(selectAuthUser);
   const rows = useAppSelector((state) => state.leaderboard.maxWpm);
+
+  console.log('rows', rows);
 
   useEffect(() => {
     const fetchWpmLeaderboards = async () => {
@@ -45,80 +54,41 @@ const Leaderboards = () => {
     fetchWpmLeaderboards();
   }, []);
 
+  const renderCell = useCallback((row: LeaderboardRow, columnKey: Key) => {
+    switch (columnKey) {
+      default:
+        return <p className="text-left text-base">{row[columnKey]}</p>;
+    }
+  }, []);
+
   return (
     <div className="mt-12 text-center text-gray-300">
-      <h1 className="mb-4 text-5xl">Leaderboards</h1>
-      <h2 className="mb-4 text-2xl">Highest WPM</h2>
-      <div className="h-[75vh]">
-        <DataGrid
-          rows={rows}
-          columns={columns}
-          // onCellClick={(e) => handleRowClick(e)}
-          sx={{
-            '& .MuiDataGrid-virtualScrollerRenderZone': {
-              '& .MuiDataGrid-row': {
-                '&:nth-of-type(2n)': { backgroundColor: '#0f172a' },
-                border: 'none',
-                //   '&:nth-child(odd)': { backgroundColor: '#1e293b' },
-              },
-              '& .MuiDataGrid-row:hover': {
-                backgroundColor: '#3b403c',
-              },
-            },
-
-            '& .MuiDataGrid-columnHeaders': {
-              backgroundColor: '#059669',
-              color: '#d1d5db',
-              fontSize: 20,
-              fontWeight: 'bold',
-              border: '1px solid white',
-              borderRadius: '8px 8px 0 0',
-            },
-            //   boxShadow: 2,
-            border: 0,
-            //   fontSize: 16,
-            //   borderColor: 'primary.light',
-            // '& .MuiDataGrid-cell:hover': {
-            //   color: 'green',
-            //   cursor: 'pointer',
-            // },
-            color: 'white',
-            '& .MuiDataGrid-root': {
-              border: '1px solid white',
-            },
-            '& .MuiDataGrid-cell': {
-              borderLeft: '1px solid white',
-              borderBottom: '1px solid #404040',
-              borderCollapse: 'collapse',
-            },
-            '& .name-column--cell': {
-              color: 'blue',
-            },
-
-            '& .MuiDataGrid-virtualScroller': {
-              backgroundColor: '#0f0f0f',
-            },
-            '& .MuiDataGrid-footerContainer': {
-              borderTop: 'none',
-              border: '1px solid white',
-              backgroundColor: '#d1d5db',
-              borderRadius: '0 0 8px 8px',
-            },
-            scrollbarColor: 'red',
-            '& .MuiDataGrid-virtualScroller::-webkit-scrollbar': {
-              width: '0.4em',
-            },
-            '& .MuiDataGrid-virtualScroller::-webkit-scrollbar-track': {
-              background: '#f1f1f1',
-            },
-            '& .MuiDataGrid-virtualScroller::-webkit-scrollbar-thumb': {
-              backgroundColor: '#888',
-            },
-            '& .MuiDataGrid-virtualScroller::-webkit-scrollbar-thumb:hover': {
-              background: '#555',
-            },
+      <h1 className="mb-4 text-left text-3xl">Leaderboards</h1>
+      <div>
+        <Table
+          aria-label="Leaderboards showing top 10 scores"
+          classNames={{
+            wrapper:
+              'bg-backgroundGrey rounded-lg rounded-xl border border-gray-700',
+            th: 'bg-gray-700/70 text-base text-white',
+            tr: 'hover:bg-gray-700/50  transition-colors first:[&>td]:rounded-l-lg last:[&>td]:rounded-r-lg',
           }}
-        />
+        >
+          <TableHeader>
+            {columns.map((column) => (
+              <TableColumn key={column.uid}>{column.name}</TableColumn>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.key}>
+                {(columnKey) => (
+                  <TableCell>{renderCell(row, columnKey)}</TableCell>
+                )}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </div>
     </div>
   );
